946f3e0d3dcc4b735dce93d201214cdc0d9da44e
TomGrzembke <tomgrzembke04@gmail.com>
2023-10-08 17:55:06 +0200

    Module prep, Project settings

---
+++ Assets/+++Workdata/Scripts/Modules/ModuleInstancing.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/// <summary> Handles an array of given possible Modules to instance itself as one</summary>
+public class ModuleInstancing : MonoBehaviour
+{
+    #region serialized fields
+    GameObject[] possibleModules;
+    #endregion
+
+    #region private fields
+
+    #endregion
+
+    void Start()
+    {
+        
+    }
+}
