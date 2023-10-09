b1f76d2c577f2ccb494a3765c4ff7d6140b08327
TomGrzembke <tomgrzembke04@gmail.com>
2023-10-09 23:03:22 +0200

    Tool tip for ranomizer

--- Assets/+++Workdata/Scripts/Modules/ModuleSeeding.cs
+++ Assets/+++Workdata/Scripts/Modules/ModuleSeeding.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class ModuleSeeding : MonoBehaviour
 {
     #region serialized fields
+    [Tooltip("Randomizes the level at start when 0, loads level with given seed when not 0")]
     [SerializeField] int currentSeed;
 
     #endregion
