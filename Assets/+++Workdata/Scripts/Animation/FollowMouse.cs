61b5a8148b772b6c240d9735d0726cca99548024
TomGrzembke <tomgrzembke04@gmail.com>
2023-10-10 01:13:07 +0200

    Animation Logic and first procedural test

---
+++ Assets/+++Workdata/Scripts/Animation/FollowMouse.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    #region serialized fields
+    [SerializeField] float rotationSpeed = 25;
+
+    #endregion
+
+    #region private fields
+    Vector2 dir;
+    Quaternion rotation;
+    Camera mainCam;
+    float angle;
+    float movespeed = 10;
+    #endregion
+
+    void Awake()
+    {
+        mainCam = Camera.main;    
+    }
+
+    void Update()
+    {
+        dir = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+        rotation = Quaternion.AngleAxis(-angle - 90, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+
+        Vector2 cursorPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, cursorPos, movespeed * Time.deltaTime);
+    }
+}
