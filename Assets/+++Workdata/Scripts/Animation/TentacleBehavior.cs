bd4a68a6c7573bd022b0a5f744c60c73a8495b3f
TomGrzembke <tomgrzembke04@gmail.com>
2023-10-11 01:28:38 +0200

    Customized the TentacleBehavior script to fit our needs and validate on change

--- Assets/+++Workdata/Scripts/Animation/TentacleBehavior.cs
+++ Assets/+++Workdata/Scripts/Animation/TentacleBehavior.cs
@@ -1,39 +1,45 @@
-using System;
-using System.Collections;
+using MyBox;
 using UnityEngine;
 
 public class TentacleBehavior : MonoBehaviour
 {
     #region serialized fields
-    [SerializeField] int length;
-    [SerializeField] LineRenderer lineRend;
-    [SerializeField] Vector3[] segmentPoses;
     [SerializeField] Transform targetDir;
-    [SerializeField] float targetDist;
+    [SerializeField] PointFollowMode pointFollowMode;
+
+    [SerializeField] int length;
+    [Tooltip("Distance between created Points, will feel smoother when smaller and more stagnant whem higher")]
+    [SerializeField] float vertexDistance;
+    [Tooltip("Determines the delay of how fast the points will follow the following point")]
     [SerializeField] float smoothSpeed;
+    [ConditionalField(nameof(pointFollowMode), false, PointFollowMode.overlap), SerializeField] float trailSpeed = 350;
+
     [SerializeField] WiggleMode wiggleMode;
-    [ConditionalField("WiggleState", WiggleMode.wiggle)][SerializeField] float trailSpeed = 350;
-    [SerializeField] float wiggleSpeed = 10;
-    [SerializeField] float wiggleMagnitude = 20;
-    [SerializeField] Transform wiggleDir;
+    [ConditionalField(nameof(wiggleMode), false, WiggleMode.wiggle), SerializeField] float wiggleSpeed = 10;
+    [ConditionalField(nameof(wiggleMode), false, WiggleMode.wiggle), SerializeField] float wiggleMagnitude = 20;
+    [ConditionalField(nameof(wiggleMode), false, WiggleMode.wiggle), SerializeField] Transform wiggleDir;
     #endregion
 
     #region private fields
+    LineRenderer lineRend;
+    Vector3[] segmentPoses;
     Vector3[] segmentV;
+    Vector3 targetPos;
     #endregion
 
     #region enums/OnValidate
+    enum PointFollowMode
+    {
+        overlap,
+        stack
+    }
     enum WiggleMode
     {
         dontWiggle,
         wiggle
     }
 
-    void OnValidate()
-    {
-        if (wiggleMode == WiggleMode.wiggle)
-            
-    }
+    void Awake() => lineRend = GetComponent<LineRenderer>();
     #endregion
 
     void Start()
@@ -45,15 +51,41 @@ public class TentacleBehavior : MonoBehaviour
 
     void Update()
     {
-        if (wiggleDir != null)
-            wiggleDir.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * wiggleSpeed) * wiggleMagnitude);
+        WiggleLogic();
 
-        segmentPoses[0] = targetDir.position;
+        AttachLogic();
 
-        for (int i = 1; i < segmentPoses.Length; i++)
+        PointFollowUpLogic();
+    }
+
+    void PointFollowUpLogic()
+    {
+        if (pointFollowMode == PointFollowMode.overlap)
         {
-            segmentPoses[i] = Vector3.SmoothDamp(segmentPoses[i], segmentPoses[i - 1] + targetDir.right * targetDist, ref segmentV[i], smoothSpeed + i / trailSpeed);
+            for (int i = 1; i < segmentPoses.Length; i++)
+            {
+                segmentPoses[i] = Vector3.SmoothDamp(segmentPoses[i], segmentPoses[i - 1] + targetDir.right * vertexDistance, ref segmentV[i], smoothSpeed + i / trailSpeed);
+            }
+        }
+        else if (pointFollowMode == PointFollowMode.stack)
+        {
+            for (int i = 1; i < segmentPoses.Length; i++)
+            {
+                targetPos = segmentPoses[i - 1] + (segmentPoses[i] - segmentPoses[i - 1]).normalized * vertexDistance;
+                segmentPoses[i] = Vector3.SmoothDamp(segmentPoses[i], targetPos, ref segmentV[i], smoothSpeed);
+            }
         }
         lineRend.SetPositions(segmentPoses);
+    }
+
+    void AttachLogic()
+    {
+        segmentPoses[0] = targetDir.position;
+    }
+
+    void WiggleLogic()
+    {
+        if (wiggleDir != null && wiggleMode == WiggleMode.wiggle)
+            wiggleDir.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * wiggleSpeed) * wiggleMagnitude);
     }
 }
