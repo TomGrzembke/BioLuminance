eadcd2336e0f1847de4b22f2a0d1d7dcf311b3c8
TomGrzembke <tomgrzembke04@gmail.com>
2023-10-31 00:09:34 +0200

    Removed one slow related field of player movement

--- Assets/+++Workdata/Scripts/Character/Player/PlayerMovement.cs
+++ Assets/+++Workdata/Scripts/Character/Player/PlayerMovement.cs
@@ -13,7 +13,6 @@ public class PlayerMovement : MonoBehaviour
     #region serialized fields
     [SerializeField] float defaultSpeed;
     [SerializeField] float sprintSpeed;
-    [SerializeField] float slowPercentage;
     [SerializeField] ControlState controlState;
     #endregion
 
