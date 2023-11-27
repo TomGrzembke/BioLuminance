129126d1e32686122d5ec7747056c1c2ece3b681
Padrie <pstrehlow007@gmail.com>
2023-11-27 14:21:45 +0100

    Minimap script logic

--- Assets/+++Workdata/Scripts/UI/MinimapCreatureDots.cs
+++ Assets/+++Workdata/Scripts/UI/MinimapCreatureDots.cs
@@ -16,11 +16,9 @@ public class MinimapCreatureDots : MonoBehaviour
     public List<GameObject> GetGameObjectsInLayer(string layerName)
     {
         List<GameObject> allObjectsInLayer = new List<GameObject>();
-
-        // Find all objects of type GameObject in the scene, including inactive ones
+        
         GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
-
-        // Filter objects based on layer and activity
+        
         allObjectsInLayer = allObjects.Where(obj => obj.layer == LayerMask.NameToLayer(layerName) && obj.activeInHierarchy).ToList();
 
         Debug.Log($"<color=blue>There are {allObjectsInLayer.Count} items on the {layerName} layer.</color>");
