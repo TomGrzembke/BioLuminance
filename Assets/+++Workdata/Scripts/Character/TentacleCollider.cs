f7c8cdea502448156184245d5a6f5943374b7de9
TomGrzembke <tomgrzembke04@gmail.com>
2023-12-03 13:03:18 +0100

    Tentacle collider test 1

---
+++ Assets/+++Workdata/Scripts/Character/TentacleCollider.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+public class TentacleCollider : MonoBehaviour
+{
+    #region serialized fields
+
+    #endregion
+
+    #region private fields
+    EdgeCollider2D edgeCollider;
+    LineRenderer lineRenderer;
+    #endregion
+
+    void Awake()
+    {
+        edgeCollider = GetComponent<EdgeCollider2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        SetEdgeCollider();
+    }
+
+    void SetEdgeCollider()
+    {
+        List<Vector2> edges = new();
+        for (int i = 0; i < edges.Count; i++)
+        {
+            Vector3 lineRendererPoint = lineRenderer.GetPosition(i);
+            edges.Add(new(lineRendererPoint.x, lineRendererPoint.y));
+        }
+
+        edgeCollider.SetPoints(edges);
+    }
+}
