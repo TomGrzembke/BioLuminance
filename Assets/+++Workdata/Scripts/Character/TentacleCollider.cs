5e56e449cf916fe4687341c8a463afa024b27382
TomGrzembke <tomgrzembke04@gmail.com>
2023-12-03 13:31:30 +0100

    Fixed linerenderer following

--- Assets/+++Workdata/Scripts/Character/TentacleCollider.cs
+++ Assets/+++Workdata/Scripts/Character/TentacleCollider.cs
@@ -27,10 +27,11 @@ public class TentacleCollider : MonoBehaviour
     void SetEdgeCollider()
     {
         List<Vector2> edges = new();
-        for (int i = 0; i < edges.Count; i++)
+        for (int i = 0; i < lineRenderer.positionCount; i++)
         {
             Vector3 lineRendererPoint = lineRenderer.GetPosition(i);
             edges.Add(new(lineRendererPoint.x, lineRendererPoint.y));
+            edges[i] = transform.InverseTransformPoint(edges[i]);;
         }
 
         edgeCollider.SetPoints(edges);
