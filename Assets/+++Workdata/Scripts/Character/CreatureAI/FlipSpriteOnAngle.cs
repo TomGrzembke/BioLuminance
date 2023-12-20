b591d825fa8f3face1b06889c8d99d328149576e
TomGrzembke <tomgrzembke04@gmail.com>
2023-12-20 10:46:37 +0100

    Optimized FlipSprite script

--- Assets/+++Workdata/Scripts/Character/CreatureAI/FlipSpriteOnAngle.cs
+++ Assets/+++Workdata/Scripts/Character/CreatureAI/FlipSpriteOnAngle.cs
@@ -1,60 +1,28 @@
 using MyBox;
 using UnityEngine;
 
-/// <summary> Gets the sr and flips the flipY when angle of target is reached</summary>
+/// <summary> Flips the object with context</summary>
 public class FlipSpriteOnAngle : MonoBehaviour
 {
-    enum Flip
-    {
-        x,
-        y
-    }
-
     #region serialized fields
     [SerializeField] Transform rotationParent;
     [MinMaxRange(0, 360)]
     [SerializeField] RangedFloat flipRange = new(0, 180);
-    [SerializeField] Flip flip = Flip.y;
-    [SerializeField] bool invert;
     #endregion
+
 
     #region private fields
-    SpriteRenderer sr;
-    [SerializeField] bool flipped;
+    bool flipped;
     #endregion
 
-    void Awake()
-    {
-        sr = GetComponent<SpriteRenderer>();
-    }
     void Update()
     {
         float angle = rotationParent.rotation.eulerAngles.z;
 
         if (flipped != (angle > flipRange.Min && angle < flipRange.Max))
         {
-            print("a");
             transform.localScale = new(1, flipped ? -1 : 1, 1);
             flipped = !flipped;
         }
-
-        return;
-        if (rotationParent == null) return;
-
-
-        if (flip == Flip.x)
-        {
-            sr.flipX = angle > flipRange.Min && angle < flipRange.Max;
-            if (invert)
-                sr.flipX = !sr.flipX;
-        }
-        else if (flip == Flip.y)
-        {
-            sr.flipY = angle > flipRange.Min && angle < flipRange.Max;
-            if (invert)
-                sr.flipY = !sr.flipY;
-        }
-
-
     }
 }
