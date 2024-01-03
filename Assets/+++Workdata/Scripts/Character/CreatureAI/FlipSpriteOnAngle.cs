1f058930f49b62c35d3912a4edd9af1e41915f25
TomGrzembke <tomgrzembke04@gmail.com>
2024-01-03 12:47:18 +0100

    Flipsprite bugfix

--- Assets/+++Workdata/Scripts/Character/CreatureAI/FlipSpriteOnAngle.cs
+++ Assets/+++Workdata/Scripts/Character/CreatureAI/FlipSpriteOnAngle.cs
@@ -8,20 +8,31 @@ public class FlipSpriteOnAngle : MonoBehaviour
     [SerializeField] Transform rotationParent;
     [MinMaxRange(0, 360)]
     [SerializeField] RangedFloat flipRange = new(0, 180);
+    float angle;
     #endregion
 
     #region private fields
     bool flipped;
+    bool inFlipRange;
     #endregion
 
     void Update()
     {
-        float angle = rotationParent.rotation.eulerAngles.z;
+        FlipLogic();
+    }
 
-        if (flipped != (angle > flipRange.Min && angle < flipRange.Max))
+    void FlipLogic()
+    {
+        angle = rotationParent.rotation.eulerAngles.z;
+        inFlipRange = angle > flipRange.Min && angle < flipRange.Max;
+
+        if (flipped != inFlipRange)
         {
-            transform.localScale = new(transform.localScale.x, flipped ? -transform.localScale.y : transform.localScale.y, transform.localScale.z);
-            flipped = !flipped;
+            Vector3 localScale = transform.localScale;
+            localScale.y = -localScale.y;
+            transform.localScale = localScale;
+
+            flipped = inFlipRange;
         }
     }
 }
