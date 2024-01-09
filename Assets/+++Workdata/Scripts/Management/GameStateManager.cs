d4743b3d01eef44d50d58b0ac8df7550835e604e
TomGrzembke <tomgrzembke04@gmail.com>
2024-01-09 12:19:08 +0100

    Reload scene on death

--- Assets/+++Workdata/Scripts/Management/GameStateManager.cs
+++ Assets/+++Workdata/Scripts/Management/GameStateManager.cs
@@ -7,7 +7,7 @@ public class GameStateManager : MonoBehaviour
 {
     static GameStateManager Instance;
 
-    [SerializeField] SceneReference startScene;
+    [SerializeField] SceneReference gamePlayScene;
     [SerializeField] GameObject optionsWindow;
     [SerializeField] PauseManager pauseManager;
     void Awake() => Instance = this;
@@ -15,7 +15,7 @@ public class GameStateManager : MonoBehaviour
 
     public static void StartGame()
     {
-        Instance.StartCoroutine(Instance.LoadScenesCoroutine((int)Scenes.MainMenu, Instance.GetSceneID(Instance.startScene)));
+        Instance.StartCoroutine(Instance.LoadScenesCoroutine((int)Scenes.MainMenu, Instance.GetSceneID(Instance.gamePlayScene)));
     }
 
     public static void OptionsWindow(bool status = true)
@@ -27,6 +27,11 @@ public class GameStateManager : MonoBehaviour
     public static void GoToMainMenu()
     {
         Instance.StartCoroutine(Instance.LoadScenesCoroutine(SceneManager.GetActiveScene().buildIndex, (int)Scenes.MainMenu));
+    }
+
+    public static void ReloadGameScene()
+    {
+        Instance.StartCoroutine(Instance.ReloadGameSceneCoroutine());
     }
 
     /// <summary> Depends on the naming (0_Scene), since its gets the first char and ints it</summary>
@@ -41,6 +46,16 @@ public class GameStateManager : MonoBehaviour
         LoadingScreen.Show(this);
         yield return SceneLoader.Instance.LoadSceneViaIndex(newScene);
         yield return SceneLoader.Instance.UnloadSceneViaIndex(oldScene);
+        LoadingScreen.Hide(this);
+    }
+
+    IEnumerator ReloadGameSceneCoroutine()
+    {
+        LoadingScreen.Show(this);
+        SkillManager.CloseSkillManager();
+        yield return new WaitForSeconds(.7f);
+        yield return SceneLoader.Instance.UnloadSceneViaIndex(GetSceneID(Instance.gamePlayScene));
+        yield return SceneLoader.Instance.LoadSceneViaIndex(GetSceneID(Instance.gamePlayScene));
         LoadingScreen.Hide(this);
     }
 }
