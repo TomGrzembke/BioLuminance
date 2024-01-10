19908a2f4b2c99c051da2848baa2551d94d2367f
Padrie <pstrehlow007@gmail.com>
2024-01-10 15:51:42 +0100

    code preparation for light dimming when the player goes deeper

--- Assets/+++Workdata/Scripts/Environment/DepthLighting.cs
+++ Assets/+++Workdata/Scripts/Environment/DepthLighting.cs
@@ -1,28 +1,62 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MyBox;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
 
 public class DepthLighting : MonoBehaviour
 {
     public SpriteRenderer mapSprite;
-    public float spriteVerticalSize;
     public GameObject subject;
     public float subjectVerticalPosition;
-    
-    [Header("Lighting")] 
-    public Light2D light2D;
-    public float maxLightingLevel;
-    public float minLightingLevel;
-    
+    [Space(5)] public float maxHeight;
+    public float minHeight;
+    public float clampedHeight;
+    public float testHeight;
+
+    [Header("Lighting")] public Light2D light2D;
+    public float lightLevel;
+    public float maxLightLevel;
+    public float minLightLevel;
+
     private void Awake()
     {
-        spriteVerticalSize = (mapSprite.bounds.extents.x);
+        GetSpritePos();
     }
 
     private void Update()
     {
         subjectVerticalPosition = subject.transform.position.y;
+
+        clampedHeight = subjectVerticalPosition;
+        clampedHeight = Mathf.Clamp(clampedHeight, minHeight, maxHeight);
+        
+        if (clampedHeight < 0)
+        {
+            float percent = clampedHeight / 100;
+            testHeight = percent;
+            if (testHeight < 0)
+                testHeight *= -1f;
+        }
+
+        LightInfo();
+    }
+
+    [ButtonMethod]
+    public void GetSpritePos()
+    {
+        maxHeight = mapSprite.bounds.max.y;
+        minHeight = mapSprite.bounds.min.y;
+    }
+
+    public void LightInfo()
+    {
+        lightLevel = Mathf.Clamp(lightLevel, minLightLevel, maxLightLevel);
+        light2D.intensity = Mathf.Clamp(light2D.intensity, minLightLevel, maxLightLevel);
+
+
+        light2D.intensity = lightLevel;
     }
 }
