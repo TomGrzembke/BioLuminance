3e091b2c0666c2bc3c2517ba478d816e9ee091e5
TomGrzembke <tomgrzembke04@gmail.com>
2024-01-11 12:11:27 +0100

    Creature interaction naming

--- Assets/+++Workdata/Scripts/Rewardsystem/CombatManager.cs
+++ Assets/+++Workdata/Scripts/Rewardsystem/CombatManager.cs
@@ -1,6 +1,6 @@
+using MyBox;
 using System;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class CombatManager : MonoBehaviour
@@ -16,23 +16,45 @@ public class CombatManager : MonoBehaviour
 
     public void CreatureInteraction(StatusManager statusManager)
     {
+        if (creatureInteractions.Count == 0)
+        {
+            NewEntry(statusManager);
+        }
+
         for (int i = 0; i < creatureInteractions.Count; i++)
         {
             if (creatureInteractions[i].targetStatusManager != statusManager)
             {
-                Interaction newInteraction = new() { targetStatusManager = statusManager };
-                creatureInteractions.Add(newInteraction);
+                NewEntry(statusManager);
             }
             else if (creatureInteractions[i].targetStatusManager == statusManager)
                 creatureInteractions[i].interactedTimes++;
         }
     }
 
+    void NewEntry(StatusManager statusManager)
+    {
+        Interaction newInteraction = new() { targetStatusManager = statusManager };
+        newInteraction.SetName();
+        creatureInteractions.Add(newInteraction);
+    }
+
     [Serializable]
     class Interaction
     {
+        [HideInInspector] public string creatureName;
         public StatusManager targetStatusManager;
+
+
         public int interactedTimes;
         public float interactionCooldwon;
+
+        public void SetName()
+        {
+            if (creatureName.IsNullOrEmpty())
+            {
+                creatureName = targetStatusManager.CreatureType.ToString();
+            }
+        }
     }
 }
