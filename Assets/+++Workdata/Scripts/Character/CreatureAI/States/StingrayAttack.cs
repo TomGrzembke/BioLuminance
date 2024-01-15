7e0edf082e4e95f547c7d1912536c7cd115b0d36
TomGrzembke <tomgrzembke04@gmail.com>
2024-01-15 14:19:07 +0100

    Stingray

---
+++ Assets/+++Workdata/Scripts/Character/CreatureAI/States/StingrayAttack.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class StingrayAttack : State
+{
+    #region serialized fields
+
+    #endregion
+
+    #region private fields
+
+    #endregion
+
+    public override State SwitchStateInternal()
+    {
+        return this;
+    }
+
+    protected override void EnterInternal()
+    {
+
+    }
+
+    protected override void ExitInternal()
+    {
+
+    }
+
+    protected override void FixedUpdateInternal()
+    {
+
+    }
+
+    protected override void UpdateInternal()
+    {
+
+    }
+}
