9ad1240c1266d7580a34d29aaef6a870b1a1f149
TomGrzembke <tomgrzembke04@gmail.com>
2024-01-17 12:51:49 +0100

    OnDeath bug

--- Assets/+++Workdata/Scripts/Character/CreatureAI/States/DeathState.cs
+++ Assets/+++Workdata/Scripts/Character/CreatureAI/States/DeathState.cs
@@ -1,9 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DeathState : State
 {
     #region serialized fields
-    [SerializeField] StateManager stateManager;
+    [SerializeField] float deathTime = 5;
+    [SerializeField] Transform gfxTrans;
+    [SerializeField] Transform headTrans;
+    [SerializeField] Animator anim;
+    [SerializeField] GameObject[] objToDisableFirst;
+    [SerializeField] GameObject[] objToDisableLast;
+    [SerializeField] MonoBehaviour[] scriptsToDisable;
+
     #endregion
 
     #region private fields
@@ -12,12 +22,17 @@ public class DeathState : State
 
     public override State SwitchStateInternal()
     {
+
         return this;
     }
 
     protected override void EnterInternal()
     {
-        stateManager.enabled = false;
+        if (gfxTrans != null && headTrans != null)
+            gfxTrans.SetParent(headTrans);
+
+        StartCoroutine(DeathCoroutine());
+        StartCoroutine(SlowDownAnimCoroutine());
     }
 
     protected override void ExitInternal()
@@ -30,5 +45,43 @@ public class DeathState : State
 
     protected override void UpdateInternal()
     {
+    }
+
+    IEnumerator DeathCoroutine()
+    {
+
+        for (int i = 0; i < objToDisableFirst.Length; i++)
+        {
+            if (objToDisableFirst[i])
+                objToDisableFirst[i].SetActive(false);
+        }
+
+        List<MonoBehaviour> list = scriptsToDisable.ToList();
+        list.RemoveAll(x => x == null);
+        for (int i = 0; i < scriptsToDisable.Length; i++)
+        {
+            scriptsToDisable[i].enabled = false;
+        }
+
+        yield return new WaitForSeconds(deathTime);
+
+
+        for (int i = 0; i < objToDisableLast.Length; i++)
+        {
+            if (objToDisableLast[i])
+                objToDisableLast[i].SetActive(false);
+        }
+    }
+
+    IEnumerator SlowDownAnimCoroutine()
+    {
+        float slowTime = 0;
+        if (anim)
+            while (slowTime < deathTime)
+            {
+                slowTime += Time.deltaTime;
+                anim.speed *= -(-1 + slowTime / deathTime);
+                yield return null;
+            }
     }
 }
