c29c32d5c0d69a370493176372b267eb4e16ced3
Padrie <pstrehlow007@gmail.com>
2024-01-18 11:46:21 +0100

    Arrow on minimap that always points to the boss (sunfish)

--- Assets/+++Workdata/Scripts/VFX/Godrays.cs
+++ Assets/+++Workdata/Scripts/VFX/Godrays.cs
@@ -8,16 +8,17 @@ using Random = UnityEngine.Random;
 
 public class Godrays : MonoBehaviour
 {
-    public float spawnRange;
-    public GameObject[] godRayArray;
-    
-    float finalAngle;
-    Vector3 startAngle; // Reference to the object's original angle values
-    
-    public Vector2 plusX;
-    public Vector2 minusX;
+    [Header("Godray Data")] [SerializeField]
+    float spawnRange;
 
-    public float randomIntensity;
+    [SerializeField] GameObject[] godRayArray;
+    [SerializeField] float finalAngle;
+    [SerializeField] GameObject player;
+
+    Vector3 startAngle;
+    Vector2 plusX;
+    Vector2 minusX;
+    float randomIntensity;
 
     private void Awake()
     {
@@ -35,17 +36,18 @@ public class Godrays : MonoBehaviour
     {
         plusX = new Vector2(spawnRange / 2, transform.localPosition.y);
         minusX = new Vector2(spawnRange / -2, transform.localPosition.y);
-        
+
         for (int i = 0; i < godRayArray.Length; i++)
         {
             var spawnpoint = Random.Range(plusX.x, minusX.x);
-            var g = Instantiate(godRayArray[i], new Vector3(spawnpoint, transform.localPosition.y), new Quaternion(0,0,180,0));
+            var g = Instantiate(godRayArray[i], new Vector3(spawnpoint, transform.localPosition.y),
+                new Quaternion(0, 0, 180, 0));
             g.transform.parent = gameObject.transform;
 
             foreach (Transform child in transform)
             {
                 var light = child.GetComponent<Light2D>();
-                
+
                 randomIntensity = Random.Range(0.1f, 0.3f);
                 float randomHeight = Random.Range(100f, 200f);
 
