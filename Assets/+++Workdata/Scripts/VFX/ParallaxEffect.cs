7efa2ea063005b2a7f14c2ebd8a37b36b4da3f3b
TomGrzembke <tomgrzembke04@gmail.com>
2024-01-19 13:44:58 +0100

    More plants rigged

--- Assets/+++Workdata/Scripts/VFX/ParallaxEffect.cs
+++ Assets/+++Workdata/Scripts/VFX/ParallaxEffect.cs
@@ -1,3 +1,4 @@
+using MyBox;
 using UnityEngine;
 
 
@@ -6,39 +7,28 @@ public class ParallaxEffect : MonoBehaviour
     #region serialized fields
     [SerializeField] float amountOfParallax;
     [SerializeField] Transform camTrans;
-    [SerializeField] SpriteRenderer spriteRenderer;
 
     #endregion
 
     #region private fields
     float startingPos;
-    float lengthOfSprite;
-
+    SpriteRenderer sr;
     #endregion
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     void Start()
     {
         startingPos = transform.position.x;
-        lengthOfSprite = spriteRenderer.bounds.size.x;
     }
 
     void Update()
     {
-        Vector3 pos = camTrans.position;
-        float tempDistance = pos.x * (1 - amountOfParallax);
-        float distance = pos.x * amountOfParallax;
+        float Xpos = camTrans.position.x * amountOfParallax;
 
-        Vector3 NewPosition = new (startingPos + distance, transform.position.y, transform.position.z);
-
-        transform.position = NewPosition;
-
-        if (tempDistance > startingPos + (lengthOfSprite / 2))
-        {
-            startingPos += lengthOfSprite;
-        }
-        else if (tempDistance < startingPos - (lengthOfSprite / 2))
-        {
-            startingPos -= lengthOfSprite;
-        }
+        transform.position = transform.position.SetX(Xpos + startingPos);
     }
 }
