42e2ea4730b5ee5057f10a7da21d74a5f14ae9a2
TomGrzembke <tomgrzembke04@gmail.com>
2024-01-20 21:36:43 +0100

    Added stun particle and sunfish stun immunity

--- Assets/+++Workdata/Scripts/Character/CreatureAI/States/StunState.cs
+++ Assets/+++Workdata/Scripts/Character/CreatureAI/States/StunState.cs
@@ -1,7 +1,10 @@
+using System.Linq;
+using UnityEngine;
+
 public class StunState : State
 {
     #region serialized fields
-
+    [SerializeField] ParticleSystem[] stunSystems;
     #endregion
 
     #region private fields
@@ -16,10 +19,20 @@ public class StunState : State
     protected override void EnterInternal()
     {
         creatureLogic.agent.velocity = new(0, 0, 0);
+        if(stunSystems.Length > 0)
+            for (int i = 0; i < stunSystems.Length; i++)
+            {
+                stunSystems[i].Play();
+            }
     }
 
     protected override void ExitInternal()
     {
+        if (stunSystems.Length > 0)
+            for (int i = 0; i < stunSystems.Length; i++)
+            {
+                stunSystems[i].Stop();
+            }
     }
 
     protected override void FixedUpdateInternal()
