30eef0b9e0ba6e505ef094aa03bb2084631f7563
TomGrzembke <tomgrzembke04@gmail.com>
2024-01-20 14:50:55 +0100

    Information field cleanup

--- Assets/+++Workdata/Scripts/UI/InformationfieldPivot.cs
+++ Assets/+++Workdata/Scripts/UI/InformationfieldPivot.cs
@@ -1,42 +1,35 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using MyBox;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class InformationfieldPivot : MonoBehaviour
 {
-    public RectTransform rectTransform;
-    public RectTransform border;
-    public Canvas canvas;
-    public RectTransform canvasRect;
-    public float height;
-    public float width;
+    [SerializeField] RectTransform rectTransform;
+    [SerializeField] RectTransform border;
+    [SerializeField] Canvas canvas;
+    [SerializeField] RectTransform canvasRect;
+    [SerializeField] float height;
+    [SerializeField] float width;
 
-    private void Awake()
+    void Awake()
     {
         canvas = FindObjectOfType<Canvas>();
         rectTransform = GetComponent<RectTransform>();
     }
 
-    private void Start()
+    void Start()
     {
         canvasRect = canvas.GetComponent<RectTransform>();
         height = canvasRect.rect.height;
         width = canvasRect.rect.width;
     }
 
-    private void Update()
+    void Update()
     {
         if (border.position.x > width / 2 || border.position.y > height / 2)
         {
-            print("a");
             rectTransform.pivot = new Vector2(1, 1);
         }
         else if (border.position.x < width / 2 || border.position.y < height / 2)
         {
-            print("b");
             rectTransform.pivot = new Vector2(0, 0);
         }
     }
