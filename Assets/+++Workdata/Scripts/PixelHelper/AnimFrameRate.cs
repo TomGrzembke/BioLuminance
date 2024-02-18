f4daa99865942dd1ba4551d6dcd347da1d1a6917
TomGrzembke <tomgrzembke04@gmail.com>
2024-02-18 01:54:28 +0100

    Working framerate limiter for animations

--- Assets/+++Workdata/Scripts/PixelHelper/AnimFrameRate.cs
+++ Assets/+++Workdata/Scripts/PixelHelper/AnimFrameRate.cs
@@ -1,60 +1,34 @@
-using System.Collections;
 using UnityEngine;
 
 public class AnimFrameRate : MonoBehaviour
 {
-    [SerializeField] Animator anim;
-    [SerializeField] int animFPS = 12;
-    float frameAlpha = 0.072f;
-    float savedAnimSpeed;
-    float lastRenderTime = 0;
+    Animator anim;
     [SerializeField] int frameRate = 12;
+    float lastRenderTime = 0;
+    float savedAnimSpeed;
+    string clipName;
 
-    [SerializeField] float _frameAlpha;
-    [SerializeField] float fpsPercent;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Start()
     {
         savedAnimSpeed = anim.speed;
-        StartCoroutine(FrameCycle());
+        clipName = anim.GetCurrentAnimatorClipInfo(0)[0].clip.name;
     }
 
     void Update()
     {
         if (Time.time - lastRenderTime > 1f / frameRate)
         {
+            float clipLength = anim.GetCurrentAnimatorClipInfo(0)[0].clip.length;
+            float clipProgressionAlpha = anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+
             lastRenderTime = Time.time;
-            
+            anim.Play(clipName, 0, clipProgressionAlpha + 1f / frameRate / clipLength * savedAnimSpeed);
+            anim.speed = 0;
         }
-    }
-
-    IEnumerator FrameCycle()
-    {
-        _frameAlpha = animFPS * 0.6f / 100;
-        fpsPercent = Mathf.Clamp(Mathf.RoundToInt(animFPS * 1.667f), 0, 100);
-        float fpsAlpha = (fpsPercent / 100);
-        float frameDifference = 60 - animFPS;
-        float animClipLength = anim.GetCurrentAnimatorClipInfo(0)[0].clip.length;
-        float _frameAlpha1 = animClipLength / anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
-
-
-        for (int i = 0; i < frameDifference; i++)
-        {
-            yield return null;
-        }
-
-        anim.speed = savedAnimSpeed;
-        anim.Play(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name, 0, anim.GetCurrentAnimatorStateInfo(0).normalizedTime + _frameAlpha);
-
-        yield return null;
-        anim.speed = 0;
-
-        for (int i = 0; i < animFPS - 1; i++)
-        {
-            yield return null;
-        }
-
-
-        StartCoroutine(FrameCycle());
     }
 }
