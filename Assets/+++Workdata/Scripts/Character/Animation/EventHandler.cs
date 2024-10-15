c697a9bb477195d428fda2d53cf0cdda0b9a0af7
TomGrzembke <tomgrzembke04@gmail.com>
2024-10-15 20:54:13 +0100

    EventHandler changes start

--- Assets/+++Workdata/Scripts/Character/Animation/EventHandler.cs
+++ Assets/+++Workdata/Scripts/Character/Animation/EventHandler.cs
@@ -1,12 +1,14 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-/// <summary> This script should be attached to an animated Object to utilize the Unity Events through this or used as a trigger </summary>
+/// <summary> This script should be attached to an animated Object to utilize the Unity Events through this </summary>
 public class EventHandler : MonoBehaviour
 {
     [SerializeField] UnityEvent onEvent1;
     [SerializeField] UnityEvent onEvent2;
+    [SerializeField] UnityEvent[] onEvent;
 
     /// <summary> This can be called from animation events or from other scripts </summary>
     public void OnEvent1()
@@ -14,6 +16,10 @@ public class EventHandler : MonoBehaviour
         onEvent1.Invoke();
     }
     public void OnEvent2()
+    {
+        onEvent2.Invoke();
+    }
+    public void OnEvent(int eventID)
     {
         onEvent2.Invoke();
     }
