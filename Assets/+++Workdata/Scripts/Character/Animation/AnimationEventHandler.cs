ce1bdca8e3f0832d335b728960fdb5e5eaac4d36
TomGrzembke <tomgrzembke04@gmail.com>
2024-10-16 21:53:27 +0100

    Note functionality on animationevents added

--- Assets/+++Workdata/Scripts/Character/Animation/AnimationEventHandler.cs
+++ Assets/+++Workdata/Scripts/Character/Animation/AnimationEventHandler.cs
@@ -1,3 +1,4 @@
+using MyBox;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -5,11 +6,23 @@ using UnityEngine.Events;
 public class AnimationEventHandler : MonoBehaviour
 {
     [SerializeField] UnityEvent[] onEvent;
+    [SerializeField] AnimationUnityEvent[] onEvents;
 
     /// <param name="eventID"> The ID of the corresponding onEvent array ID</param>
     public void OnEvent(int eventID)
     {
         onEvent[eventID].Invoke();
+    }
+
+    /// <param name="eventID"> The ID of the corresponding onEvent array ID</param>
+    public void OnEvents(AnimationEvent _animEvent)
+    {
+        if (onEvents.Length <= _animEvent.intParameter) return;
+
+        onEvents[_animEvent.intParameter].animEvent?.Invoke();
+
+        if (onEvents[_animEvent.intParameter].eventNote == "")
+            onEvents[_animEvent.intParameter].eventNote = _animEvent.stringParameter;
     }
 
     /// <summary> Be careful with statemachine blending and instantiating! Might need a spawn limit per frame bunch</summary>
@@ -19,3 +32,10 @@ public class AnimationEventHandler : MonoBehaviour
     }
 }
 
+[System.Serializable]
+public struct AnimationUnityEvent
+{
+    [Tooltip("To keep track of the animation functionality")]
+    public string eventNote;
+    public UnityEvent animEvent;
+}
