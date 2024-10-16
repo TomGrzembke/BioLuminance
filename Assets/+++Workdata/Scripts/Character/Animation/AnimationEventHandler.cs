e7d70dcc8ed8aa4640834ddadb82f07d264a23ed
TomGrzembke <tomgrzembke04@gmail.com>
2024-10-16 22:34:04 +0100

    Added the option for comments on the animation events itself

--- Assets/+++Workdata/Scripts/Character/Animation/AnimationEventHandler.cs
+++ Assets/+++Workdata/Scripts/Character/Animation/AnimationEventHandler.cs
@@ -1,28 +1,34 @@
-using MyBox;
 using UnityEngine;
 using UnityEngine.Events;
 
 /// <summary> This script should be attached to an animated Object to utilize the Unity Events through this </summary>
 public class AnimationEventHandler : MonoBehaviour
 {
-    [SerializeField] UnityEvent[] onEvent;
     [SerializeField] AnimationUnityEvent[] onEvents;
 
     /// <param name="eventID"> The ID of the corresponding onEvent array ID</param>
-    public void OnEvent(int eventID)
+    public void OnAnimEventID(int eventID)
     {
-        onEvent[eventID].Invoke();
+        onEvents[eventID].animEvent?.Invoke();
     }
 
     /// <param name="eventID"> The ID of the corresponding onEvent array ID</param>
-    public void OnEvents(AnimationEvent _animEvent)
+    public void OnAnimEvent(AnimationEvent _animEvent)
     {
-        if (onEvents.Length <= _animEvent.intParameter) return;
+        int animeventID = DetermineAnimEventID(_animEvent);
 
-        onEvents[_animEvent.intParameter].animEvent?.Invoke();
+        if (onEvents.Length <= animeventID) return;
 
-        if (onEvents[_animEvent.intParameter].eventNote == "")
-            onEvents[_animEvent.intParameter].eventNote = _animEvent.stringParameter;
+        onEvents[animeventID].animEvent?.Invoke();
+
+        if (onEvents[animeventID].eventNote == "")
+            onEvents[animeventID].eventNote = _animEvent.stringParameter;
+    }
+
+    /// <summary> prefers the float parameter on an animEvent if its set, else takes the int parameter </summary>
+    int DetermineAnimEventID(AnimationEvent _animEvent)
+    {
+        return _animEvent.intParameter != 0 ? _animEvent.intParameter : (int)_animEvent.floatParameter;
     }
 
     /// <summary> Be careful with statemachine blending and instantiating! Might need a spawn limit per frame bunch</summary>
